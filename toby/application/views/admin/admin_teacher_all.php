963f197f753053d67d38ad9dfc2bad0624ffaf21
hhk <1361383493@qq.com>
2017-11-09 20:37:44 +0800

    修正页面
    改了教师名单显示

--- toby/application/views/admin/admin_teacher_all.php
+++ toby/application/views/admin/admin_teacher_all.php
@@ -5,9 +5,22 @@
 		<title></title>
 	</head>
 	<body style="background:background-color;background-color: transparent;font-size: 18px;">
-		{% for v in data %}  
-            姓名:{{ v.name }}<br>
-            密码:{{ v.password }}<br>
-        {% endfor %}  
+        <form method="" action="">
+        	<table border="2" align="center" style="margin-top: 100px;">
+        		<thead>
+        			<tr>
+        				<th width="100px">姓名</th>
+        				<th width="100px">密码</th>
+        				
+        			</tr>
+        		</thead>
+        		<tbody>
+				{% for v in data %}
+        			<td>{{ v.name }}</td>
+        			<td>{{ v.password }}</td>
+				{% endfor %}
+        		</tbody>
+        	</table>
+        </form>
 	</body>
 </html>
--- toby/application/views/teacher/teacher_exam_end.php
+++ toby/application/views/teacher/teacher_exam_end.php
@@ -8,19 +8,21 @@
 	<form >
 		
 		<table align="center" border="1" style="border:double">
+			<thead>
 			<tr>
-				<td width="90px" align="center">考试名称</td>
-				<td width="90px" align="center">考试时间</td>
-				<td width="60px" align="center">创建人</td>
-				<td width="70px" align="center">上传试卷</td>
-				<td width="70px" align="center">自动开始</td>
-				<td width="50px" align="center">进行中</td>
-				<td width="50px" align="center">已结束</td>
-				<td width="50px" align="center">已归档</td>
-				<td width="50px" align="center">已清理</td>
-				<td width="50px" align="center">编辑</td>
+				<th width="90px" align="center">考试名称</th>
+				<th width="90px" align="center">考试时间</th>
+				<th width="60px" align="center">创建人</th>
+				<th width="70px" align="center">上传试卷</th>
+				<th width="70px" align="center">自动开始</th>
+				<th width="50px" align="center">进行中</th>
+				<th width="50px" align="center">已结束</th>
+				<th width="50px" align="center">已归档</th>
+				<th width="50px" align="center">已清理</th>
+				<th width="50px" align="center">编辑</th>
 			</tr>
-			
+			</thead>
+			<tbody>
 			<tr height="20px">
 				<td></td>
 				<td></td>
@@ -33,6 +35,7 @@
 				<td></td>	
 				<td></td>
 			</tr>
+			</tbody>
 		</table>
 	</form>
     
