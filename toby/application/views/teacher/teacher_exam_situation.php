b19c871068b49dacf7d5533b4231165086a5288d
haomengyao <1728083393@qq.son>
2017-12-01 18:13:18 +0800

    考试状况

--- toby/application/views/teacher/teacher_exam_situation.php
+++ toby/application/views/teacher/teacher_exam_situation.php
@@ -3,33 +3,61 @@
 	<head>
 		<meta charset="UTF-8">
 		<title></title>
+		<link href="../application/style/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
 	</head>
     <body style="background:background-color;background-color: transparent;">
-	<form method=" " action="">
-		<h3 align="center"> 考试进行情况</h3>
-		<table align="center" border="1" style="border:double">
-			<tr>
-				<td width="200px" align="center">考试名称</td>
-				<td width="100px"></td>
-			</tr>
-			<tr>
-				<td width="200px" align="center">参加考试人数</td>
-				<td width="100px"></td>
-			</tr>
-				<td width="90px" align="center">已登录人数</td>
-				<td width="100px"></td>
-			</tr>
-				<td width="200px" align="center">未登录人数</td>
-				<td width="100px"></td>
-			</tr>
-				<td width="200px" align="center">登陆后提交答案人数</td>
-				<td width="100px"></td>
-			</tr>
-				<td width="200px" align="center">登录却未提交答案的人数</td>
-				<td width="100px"></td>
-		    </tr>
-	   </table>	
-	</form>
-    
+    <div class="panel panel-danger">
+        <div class="panel-heading">
+           <h3 class="panel-title text-center">考试进行情况</h3>
+        </div>
+        <div class="panel-body" style="line-height: 35px;">	
+		    
+			
+			<table class="table table-striped">
+				<!-- <tr>
+					<td width="200px" align="center">考试名称</td>
+					<td width="100px"></td>
+				</tr>
+				<tr>
+					<td width="200px" align="center">参加考试人数</td>
+					<td width="100px"></td>
+				</tr>
+					<td width="90px" align="center">已登录人数</td>
+					<td width="100px"></td>
+				</tr>
+					<td width="200px" align="center">未登录人数</td>
+					<td width="100px"></td>
+				</tr>
+					<td width="200px" align="center">登陆后提交答案人数</td>
+					<td width="100px"></td>
+				</tr>
+					<td width="200px" align="center">登录却未提交答案的人数</td>
+					<td width="100px"></td>
+			    </tr> -->
+
+			    <thead>
+        <tr>
+				<th >考试名称</th>
+				<th >参加考试人数</th>
+				<th >已登录人数</th>
+				<th >未登录人数</th>
+				<th >登陆后提交答案人数</th>
+				<th >登录却未提交答案的人数</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>java</th>
+          <th>90</th>
+          <th>80</th>
+          <th>10</th>
+          <th>45</th>
+          <th>35</th>
+        </tr>
+      </tbody>
+		    </table>	
+		   
+		   </div>
+		 </div>
     </body>
 </html>
